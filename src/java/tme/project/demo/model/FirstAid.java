f4906df006499dee083f6d395d397e8224c9d326
Namwvsmong100541 <wanvisa.mong@mail.kmutt.ac.th>
2019-04-18 14:47:36 +0700

    Create FirstAid Model

---
+++ src/java/tme/project/demo/model/FirstAid.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tme.project.demo.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class FirstAid {
+    private int firstAidId;
+    private String typeOfEmergency;
+    private String Description;
+
+
+    public FirstAid() {
+    }
+
+    public FirstAid(int firstAidId, String typeOfEmergency, String Description) {
+        this.firstAidId = firstAidId;
+        this.typeOfEmergency = typeOfEmergency;
+        this.Description = Description;
+    }
+
+    public int getFirstAidId() {
+        return this.firstAidId;
+    }
+
+    public void setFirstAidId(int firstAidId) {
+        this.firstAidId = firstAidId;
+    }
+
+    public String getTypeOfEmergency() {
+        return this.typeOfEmergency;
+    }
+
+    public void setTypeOfEmergency(String typeOfEmergency) {
+        this.typeOfEmergency = typeOfEmergency;
+    }
+
+    public String getDescription() {
+        return this.Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public FirstAid firstAidId(int firstAidId) {
+        this.firstAidId = firstAidId;
+        return this;
+    }
+
+    public FirstAid typeOfEmergency(String typeOfEmergency) {
+        this.typeOfEmergency = typeOfEmergency;
+        return this;
+    }
+
+    public FirstAid Description(String Description) {
+        this.Description = Description;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof FirstAid)) {
+            return false;
+        }
+        FirstAid firstAid = (FirstAid) o;
+        return firstAidId == firstAid.firstAidId && Objects.equals(typeOfEmergency, firstAid.typeOfEmergency) && Objects.equals(Description, firstAid.Description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstAidId, typeOfEmergency, Description);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " firstAidId='" + getFirstAidId() + "'" +
+            ", typeOfEmergency='" + getTypeOfEmergency() + "'" +
+            ", Description='" + getDescription() + "'" +
+            "}";
+    }
+
+}
