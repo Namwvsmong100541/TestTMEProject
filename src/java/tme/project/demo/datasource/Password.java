4163177f2e88430efb9c7c985bf6046b9478cfd5
Namwvsmong100541 <wanvisa.mong@mail.kmutt.ac.th>
2019-04-18 15:12:23 +0700

    Create Password.java

---
+++ src/java/tme/project/demo/datasource/Password.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tme.project.demo.datasource;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Password {
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA256";
+    private static final int ITERATION_COUNT = 10000;
+    private static final int KEY_LENGTH = 256;
+    private static final String SALT = "akfhsjdiropxhrj;'=-)";
+    private static final int LENGHT = 20;
+ 
+    private static String getSafetyPassword(String password) {
+ 
+        char[] passCharAry = password.toCharArray();
+        byte[] hashedSalt = getHashedSalt(SALT);
+ 
+        PBEKeySpec keySpec = new PBEKeySpec(passCharAry, hashedSalt, ITERATION_COUNT, KEY_LENGTH);
+ 
+        SecretKeyFactory skf;
+        try {
+            skf = SecretKeyFactory.getInstance(ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+ 
+        SecretKey secretKey;
+        try {
+            secretKey = skf.generateSecret(keySpec);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+        byte[] passByteAry = secretKey.getEncoded();
+ 
+        StringBuilder sb = new StringBuilder(64);
+        for (byte b : passByteAry) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        return sb.toString();
+    }
+ 
+    private static byte[] getHashedSalt(String SALT) {
+        MessageDigest messageDigest;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        messageDigest.update(SALT.getBytes());
+        return messageDigest.digest();
+    }
+    public static String getKeepPassword(String password){       
+        return Password.getSafetyPassword(password).substring(64-LENGHT);
+    }
+}
